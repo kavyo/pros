24bdada7b400e570bb6f301098b89dd9cf9f2e62
kavyo <kavipriyaurmila@gmail.com>
2017-06-22 15:01:11 +0530

    Create pro1.java

---
+++ pro1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class pro1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=(x*y)-1;
+        System.out.println(z);
+    }
+}
